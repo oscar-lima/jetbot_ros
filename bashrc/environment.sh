147b6d3d9e80be358ac482e76ecd55e595aae945
Daniela Sanchez <daniela.s11@hotmail.com>
2022-02-19 08:15:15 -0800

    add bashrc configuration

---
+++ bashrc/environment.sh
@@ -0,0 +1,4 @@
+export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}'
+export ROS_WORKSPACE=$HOME/workspace/catkin_ws/src
+export PYTHONPATH=/opt/ros/noetic/lib/python3.8/site-packages:/home/jetbot/workspace/catkin_ws/devel/lib/python3/dist-packages
+export ROS_IP=192.168.0.192
